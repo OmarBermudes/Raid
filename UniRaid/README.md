6bfe1a2422d7f3d9a719f0b71ae4a24bea421a0e
OmarBermudes <omib.94@gmail.com>
2016-03-27 01:29:14 -0600

    asd

---
+++ UniRaid/README.md
@@ -0,0 +1,3 @@
+# UniRaid
+
+a [Sails](http://sailsjs.org) application
---
+++ UniRaid/api/models/Post.js
@@ -0,0 +1,42 @@
+/**
+ * Post.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+
+  	email:{
+  		type:'email',
+  		required:true
+  	},
+  	route:{
+  		type:'text',
+  		required:true
+  	},
+  	cost:{
+  		type:'integer',
+  		required:true
+  	},
+  	// Son los dtos de contacto, numero de celular etc
+  	num_contact:{
+  		type:'string'
+  	},
+  	// types es el tipo de raid, llegada a la uni o saliendo de la uni
+  	types:{
+  		type:'boolean',
+  		required:true
+  	},
+  	sits:{
+  		type:'integer',
+  		required:true
+  	},
+  	notes:{
+  		type:'text'
+  	}
+  }
+};
+
